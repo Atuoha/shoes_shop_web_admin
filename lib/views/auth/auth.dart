a06c6de4bff595e6ef3aab4127a7b9c5677da86c
Atuoha <atuohainitiatives@gmail.com>
2023-06-05 06:15:40 +0100

    adjustment to firebase ui auth configs

--- lib/views/auth/auth.dart
+++ lib/views/auth/auth.dart
@@ -1,8 +1,10 @@
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart' hide EmailAuthProvider, PhoneAuthProvider;
+import 'package:firebase_auth/firebase_auth.dart'
+    hide EmailAuthProvider, PhoneAuthProvider;
 import 'package:firebase_ui_oauth_google/firebase_ui_oauth_google.dart';
 
+import '../../resources/assets_manager.dart';
 import '../main/main_screen.dart';
 
 class AuthenticationScreen extends StatelessWidget {
@@ -10,7 +12,6 @@ class AuthenticationScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     return StreamBuilder<User?>(
       stream: FirebaseAuth.instance.authStateChanges(),
       // If the user is already signed-in, use it as initial data
@@ -18,12 +19,42 @@ class AuthenticationScreen extends StatelessWidget {
       builder: (context, snapshot) {
         // User is not signed in
         if (!snapshot.hasData) {
-          return  SignInScreen(
-             providers: [
-               EmailAuthProvider(),
-               PhoneAuthProvider(),
-               GoogleProvider(clientId: '719326293072-q9ao67f1nolt50ev21om8bou759cr4mk.apps.googleusercontent.com'),
-             ],
+          return SignInScreen(
+            subtitleBuilder: (context, action) {
+              return Padding(
+                padding: const EdgeInsets.only(bottom: 8),
+                child: Text(
+                  action == AuthAction.signIn
+                      ? 'Welcome to ShoeShop! Please sign in to continue.'
+                      : 'Welcome to ShoeShop! Please create an account to continue',
+                ),
+              );
+            },
+            footerBuilder: (context, _) {
+              return const Padding(
+                padding: EdgeInsets.only(top: 16),
+                child: Text(
+                  'By signing in, you agree to our terms and conditions.',
+                  style: TextStyle(color: Colors.grey),
+                ),
+              );
+            },
+            sideBuilder: (context, constraints) {
+              return Padding(
+                padding: const EdgeInsets.all(20),
+                child: AspectRatio(
+                  aspectRatio: 1,
+                  child: Image.asset(AssetManager.logo, width: 100),
+                ),
+              );
+            },
+            providers: [
+              EmailAuthProvider(),
+              PhoneAuthProvider(),
+              GoogleProvider(
+                  clientId:
+                      '719326293072-q9ao67f1nolt50ev21om8bou759cr4mk.apps.googleusercontent.com'),
+            ],
           );
         }
 
